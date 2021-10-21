7de8a2f71ca83b88501e2d3bec0ffaee1b43aa4f
kasterra <kasterra@naver.com>
2021-10-21 16:34:29 +0900

    Poster stuffs

--- src/Routes/Home/HomePresenter.jsx
+++ src/Routes/Home/HomePresenter.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Section from '../../Components/Section'
 import Loader from '../../Components/Loader';
+import Message from '../../Components/Message';
+import Poster from '../../Components/Poster'
 
 const Container = styled.div`
   padding:0px 10px;
@@ -12,20 +14,47 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>(loa
   <Container>
     {upcoming && upcoming.length > 0 && (
       <Section title="Upcoming movies">
-        {upcoming.map(movie => <span key="{movie.id}">{movie.title}</span>)}
+        {upcoming.map(movie => 
+        <Poster
+          key={movie.id}
+          id={movie.id} 
+          title={movie.original_title}
+          imageUrl={movie.poster_path}
+          isMovie
+          rating={movie.vote_average}
+          year={movie.release_date && movie.release_date.substring(0,4)}
+        />)}
       </Section>
     )}
     {nowPlaying && nowPlaying.length > 0 && (
      <Section title="Now Playing">
-       {nowPlaying.map(movie => <span key="{movie.id}">{movie.title}</span>)}
+       {nowPlaying.map(movie => 
+       <Poster
+        key={movie.id}
+        id={movie.id}
+        title={movie.original_title}
+        imageUrl={movie.poster_path}
+        isMovie
+        rating={movie.vote_average}
+        year={movie.release_date && movie.release_date.substring(0,4)}
+       />)}
      </Section>
     )}
     {popular && popular.length > 0 && (
       <Section title="popular movie">
-        {popular.map(movie => <span key="{movie.id}">{movie.title}</span>)}
+        {popular.map(movie => 
+        <Poster
+          key={movie.id}
+          id={movie.id} 
+          title={movie.original_title}
+          imageUrl={movie.poster_path}
+          isMovie
+          rating={movie.vote_average}
+          year={movie.release_date && movie.release_date.substring(0,4)}
+        />)}
       </Section>
     )}
-
+    {error && <Message color="#e74c3c" text = {error}/>}
   </Container>
 );
 
--- src/Routes/Search/SearchPresenter.jsx
+++ src/Routes/Search/SearchPresenter.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Loader from '../../Components/Loader';
 import Section from '../../Components/Section';
+import Message from '../../Components/Message';
+import Poster from '../../Components/Poster';
 
 const Container = styled.div`
   padding:0px 20px;
@@ -39,14 +41,38 @@ const SearchPresenter = ({
     <>
       {movieResults && movieResults.length > 0 && (
         <Section title="Movie Results">
-          {movieResults.map(movie => <span key={movie.id}>{movie.title}</span>)}
+          {movieResults.map(movie =>        
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              isMovie
+              rating={movie.vote_average}
+              year={movie.release_date && movie.release_date.substring(0,4)}
+          />)}
         </Section>
       )}
       {tvResults && tvResults.length > 0 && (
         <Section title="TV Results">
-          {tvResults.map(tv => <span key={tv.id}>{tv.name}</span>)}
+          {tvResults.map(show =>
+            <Poster
+              key={show.id}
+              id={show.id}
+              title={show.original_name}
+              imageUrl={show.poster_path}
+              isMovie
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0,4)}
+     />)}
         </Section>
       )}
+      {error && <Message color="#e74c3c" text={error} />}
+      {tvResults && 
+      movieResults && 
+      tvResults.length === 0 &&
+      movieResults.length === 0 && 
+      <Message text="Nothing Found" color="#95a5a6"/>}
     </>
   }
 </Container>);
